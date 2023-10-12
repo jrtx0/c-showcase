6acc191fe2aca77cdb493f37ac1ea745502bb272
jrtx0 <1511198943@qq.com>
2023-10-12 08:56:11 +0000

    add leetcode c solution

---
+++ leetcode/1.两数之和.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *index = (int*)malloc(2 * sizeof(int));
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                index[0] = i;
+                index[1] = j;
+                *returnSize = 2;
+                return index;
+            }
+        }
+    }
+    return index;
+}
