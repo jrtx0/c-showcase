1529af0146378c2c0884cc5a5145dcc7132f646d
jrtx0 <1511198943@qq.com>
2023-10-13 09:46:48 +0000

    add hash solution

--- leetcode/1.两数之和.c
+++ leetcode/1.两数之和.c
@@ -1,20 +1,65 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+// 方法一：暴力枚举
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    int *index = (int*)malloc(2 * sizeof(int));
     for (int i = 0; i < numsSize; i++)
     {
         for (int j = i + 1; j < numsSize; j++)
         {
             if (nums[i] + nums[j] == target)
             {
-                index[0] = i;
-                index[1] = j;
+                int* ret = malloc(sizeof(int) * 2);
+                ret[0] = i;
+                ret[1] = j;
                 *returnSize = 2;
-                return index;
+                return ret;
             }
         }
     }
-    return index;
+    *returnSize = 0;
+    return NULL;
+}
+
+// 方法二：哈希表
+struct hashTable {
+    int key;
+    int val;
+    UT_hash_handle hh;
+};
+
+struct hashTable* hashtable;
+
+struct hashTable* find(int ikey) {
+    struct hashTable* tmp;
+    HASH_FIND_INT(hashtable, &ikey, tmp);
+    return tmp;
+}
+
+void insert(int ikey, int ival) {
+    struct hashTable* it = find(ikey);
+    if (it == NULL) {
+        struct hashTable* tmp = malloc(sizeof(struct hashTable));
+        tmp->key = ikey, tmp->val = ival;
+        HASH_ADD_INT(hashtable, key, tmp);
+    } else {
+        it->val = ival;
+    }
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    hashtable = NULL;
+    for (int i = 0; i < numsSize; i++) {
+        struct hashTable* it = find(target - nums[i]);
+        if (it != NULL) {
+            int* ret = malloc(sizeof(int) * 2);
+            ret[0] = it->val, ret[1] = i;
+            *returnSize = 2;
+            return ret;
+        }
+        insert(nums[i], i);
+    }
+    *returnSize = 0;
+    return NULL;
 }
