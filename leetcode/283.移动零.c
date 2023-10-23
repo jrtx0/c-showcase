a0c07af6ec2224eefb6d0c3ed9a90dbaaf1b86dd
jrtx0 <1511198943@qq.com>
2023-10-23 01:46:37 +0000

    update solution

--- leetcode/283.移动零.c
+++ leetcode/283.移动零.c
@@ -1,4 +1,4 @@
-// 我的解法
+// 我的解法：遇到零就整体向前移动，效率比较低，可以利用双指针等所有的零都跨过了再移动。
 void moveZeroes(int* nums, int numsSize)
 {
     int zeroCnt = 0;
@@ -37,7 +37,40 @@ void moveZeroes(int* nums, int numsSize)
     }
 }
 
-// 官方双指针
+// 我的解法优化：双指针移动直到遇到非零再进行交换
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b, *b = t;
+}
+
+void moveZeroes(int* nums, int numsSize){
+    int left = 0, right = 0;
+
+    while (right < numsSize)
+    {
+        if (nums[left] != 0 && nums[right] != 0)
+        {
+            left++;
+            right++;
+        }
+        else if (nums[left] == 0 && nums[right] == 0)
+        {
+            right++;
+        }
+        else if (nums[left] == 0 && nums[right] != 0)
+        {
+            swap(nums + left, nums + right);
+        }
+        else
+        {
+            left++;
+        }
+    }
+}
+
+
+// 双指针解法：左指针指向当前已经处理好的序列的尾部，右指针指向待处理序列的头部。
 void swap(int *a, int *b)
 {
     int t = *a;
